e945cfeaa4ee789229b78905b4ccfe53d4012e08
Jakedalus <jacquedalus@gmail.com>
2021-06-25 18:38:26 -0400

    Basic types

--- course_project/app.ts
+++ course_project/app.ts
@@ -1,1 +1,33 @@
-console.log('my code');
+// const person: {
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	role: [number, string]; // tuples
+// } = {
+// 	name: 'Jacob',
+// 	age: 39,
+// 	hobbies: [ 'sports', 'cooking' ],
+// 	role: [ 2, 'author' ]
+// };
+
+enum Role {
+	ADMIN,
+	READ_ONLY,
+	AUTHOR
+}
+
+const person = {
+	name: 'Jacob',
+	age: 39,
+	hobbies: [ 'sports', 'cooking' ],
+	role: Role.ADMIN
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+if (person.role === Role.ADMIN) {
+	console.log('Admin');
+}
+
+console.log(person);
