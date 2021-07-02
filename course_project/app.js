6c3c7be6e7e09027c9fcb0d4f73e46ff449180a0
Jakedalus <jacquedalus@gmail.com>
2021-07-02 15:42:37 -0400

    Add button, fix typo in filename

--- course_project/app.js
+++ course_project/app.js
@@ -1,2 +1,7 @@
 "use strict";
 console.log('test');
+var button = document.querySelector('button');
+console.log("button", button);
+button.addEventListener('click', function () {
+    console.log('clicked');
+});
--- course_project/app.ts
+++ course_project/app.ts
@@ -1,1 +1,9 @@
 console.log('test');
+
+const button = document.querySelector('button')!;
+
+console.log(`button`, button);
+
+button.addEventListener('click', () => {
+	console.log('clicked');
+});
